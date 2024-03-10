a96070a9814c60c84bc2e8d47cc4997b2dfab08a
kien.mac <mactrungkien2000@gmail.com>
2024-03-10 17:55:07 +0700

    Update ports in docker-compose.yml and add constant.ts file

---
+++ src/utils/constant.ts
@@ -0,0 +1,5 @@
+export const EXTERNAL_URLS = {
+  TRANSACTION_POOL: "http://127.4.240.104:3001/transaction-pool",
+  TRANSACTION: "http://127.4.240.104:3001/transaction",
+  CONTRACT: "http://127.4.240.104:3001/contract",
+};
