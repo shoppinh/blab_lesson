f9a12456eafe7e2e83481e346b38ed8e80a93cdc
kien.mac <mactrungkien2000@gmail.com>
2024-03-12 22:34:55 +0700

    Update external URLs in constant.ts

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,5 +1,5 @@
 export const EXTERNAL_URLS = {
-  TRANSACTION_POOL: "http://127.4.240.104:3001/transaction-pool",
-  TRANSACTION: "http://127.4.240.104:3001/transaction",
-  CONTRACT: "http://127.4.240.104:3001/contract",
+  TRANSACTION_POOL: "http://127.4.240.104:3000/transaction-pool",
+  TRANSACTION: "http://127.4.240.104:3000/transaction",
+  CONTRACT: "http://127.4.240.104:3000/contract",
 };
